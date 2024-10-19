6afdd5efa83f0e18eb9e65f2cd214371042ab2a4
joni <jonipharju@icloud.com>
2024-10-20 01:33:52 +0300

    add prose component and tailwind typography

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,24 +12,11 @@ export default {
         // Text Colors
         textMain: "#111827", // Light mode text
         textMainDark: "#e5e7eb", // Dark mode text
-
-        // Primary Colors
-        primary: "#6b7280", // Light mode primary
-        primaryDark: "#9ca3af", // Dark mode primary
-
-        // Accent Colors
-        accent: "#10b981", // Light mode accent (green)
-        accentDark: "#34d399", // Dark mode accent (lighter green)
-
-        // Secondary Accent Colors
-        secondaryAccent: "#3b82f6", // Light mode secondary (blue)
-        secondaryAccentDark: "#60a5fa", // Dark mode secondary (lighter blue)
-
-        // Button Background Colors
-        buttonBg: "#e5e7eb", // Light mode button background
-        buttonBgDark: "#374151", // Dark mode button background
+      },
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"], // Register Roboto as the default sans-serif font
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
